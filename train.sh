a0e3bb5e19e33eb5513448fdd03a348b4d2c87fd
ZJ-Promising <2719595166@qq.com>
2026-06-21 15:53:09 +0800

    Modify training parameters and output log path

--- train.sh
+++ train.sh
@@ -3,21 +3,21 @@
 # Training script for Train_pvt.py with nohup support
 
 # Default parameters (can be modified as needed)
-EPOCH=300
+EPOCH=20
 LR=1e-4
 BATCHSIZE=4
 TRAINSIZE=512
 CLIP=0.5
 DECAY_RATE=0.1
-DECAY_EPOCH=80
-LOAD="model/pretrained/base_patch16_384.pth"
+DECAY_EPOCH=0
+LOAD=""
 GPU_ID="1"
 TRAIN_ROOT="../IML-DS/train/"
 VAL_ROOT="../IML-DS/test/"
 MODEL="train"
-SAVE_PATH="log/12.21_xiao_zhi-0.15/"
+SAVE_PATH=""
 
-# Run training with nohup in background
+
 nohup python -u Train.py \
     --epoch ${EPOCH} \
     --lr ${LR} \
@@ -32,7 +32,7 @@ nohup python -u Train.py \
     --val_root ${VAL_ROOT} \
     --model ${MODEL} \
     --save_path ${SAVE_PATH} \
-    > 日志/12.21_xiao_zhi-0.15.log 2>&1 &
+    > 日志/out.log 2>&1 &
 
 echo "Training started in background. Check train.log for output."  
 echo "Process ID: $!" 
